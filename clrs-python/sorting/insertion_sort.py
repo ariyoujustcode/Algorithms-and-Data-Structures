e800cb8b7810f66d3af6685ab6644a4f4ebd2591
ariyoujustcode <arimarine23@gmail.com>
2025-04-18 15:37:56 -0500

    completed insertion_sort algorithm

---
+++ clrs-python/sorting/insertion_sort.py
@@ -0,0 +1,14 @@
+def insertion_sort(arr: int):
+    for i in range(1, len(arr)):
+        value = arr[i]
+        j = i - 1
+
+        while j >= 0 and arr[j] > value:
+            arr[j + 1] = arr[j]
+            j -= 1
+
+        arr[j + 1] = value
+
+arr = [10, 5, 1, 20, 15]
+insertion_sort(arr)
+print(arr)
